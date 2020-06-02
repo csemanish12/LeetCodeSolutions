235062cc98176fb71c44d8ecf8b18b036951d52a
sah_manish <manish.sah@debajyotigroup.com>
2020-06-02 21:20:02 +0545

    delete node from linked list

---
+++ Week 1/Delete Node in linked List/solution.py
@@ -0,0 +1,8 @@
+def deleteNode(node):
+    """
+    :type node: ListNode
+    :rtype: void Do not return anything, modify node in-place instead.
+    """
+    next = node.next
+    node.val = next.val
+    node.next = next.next
